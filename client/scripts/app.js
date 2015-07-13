07a0bb0bddbe90c07c0481f9b895bc96c5c94ab6
Hack Reactor Students <accounts@hackreactor.com>
2015-07-13 12:11:11 -0700

    retrieves messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,28 @@
 // YOUR CODE HERE:
+var fetchMessages = function() {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      displayMessages(data);
+    }
+  });
+};
 
+var displayMessages = function (data) {
+  var results = data.results;
+  var user, text, time, $message;
+  var $messages = $('<div class="messages"></div>');
+  _(results).each(function(result){
+    user = result.username;
+    text = result.text;
+    time = result.updatedAt;
+    $message = $('<p></p>').text(user + ': ' + text + ' ' + time);
+    $messages.append($message);
+  });
+  $('.messages').replaceWith($messages);
+};
+
+fetchMessages();
+setInterval(fetchMessages, 2000);
